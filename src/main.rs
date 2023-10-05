a342e04af307d77b49c904b8452f2ca3f212aa63
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-10-06 05:41:09 +0700

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
-use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut _buffer: [u8; 512] = [0u8; 512];
+    let _bytes_read = stream
+        .read(&mut _buffer)
+        .expect("Failed to read the stream");
+
+    stream
+        .write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+        .expect("Failed to send response");
+}
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let conn = "127.0.0.1:4221";
+    let listener = TcpListener::bind(&conn).unwrap();
 
+    println!("Server started at {}", &conn);
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
