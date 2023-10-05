b622ae387091d4302e223e09511cafcecd90257e
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-10-06 05:55:30 +0700

    pass 3nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,40 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut _buffer: [u8; 512] = [0u8; 512];
-    let _bytes_read = stream
-        .read(&mut _buffer)
-        .expect("Failed to read the stream");
+fn get_path<'a>(lines: &'a Vec<&'a str>) -> Option<&'a str> {
+    for line in lines {
+        if line.starts_with("GET") {
+            return line.split(" ").nth(1);
+        }
+    }
 
+    None
+}
+
+fn parse_request(stream: &mut TcpStream) -> String {
+    let mut buffer: [u8; 512] = [0u8; 512];
+    let bytes_read = stream.read(&mut buffer).expect("Failed to read the stream");
+
+    return String::from_utf8_lossy(&buffer[..bytes_read]).to_string();
+}
+
+fn write_stream(stream: &mut TcpStream, message: &str) {
     stream
-        .write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
-        .expect("Failed to send response");
+        .write(message.as_bytes())
+        .expect("Failed to send a response");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let binding = parse_request(&mut stream);
+    let request_lines = binding.split("\r\n").collect::<Vec<&str>>();
+
+    let path = get_path(&request_lines).expect("Failed to get path");
+
+    if path == "/" {
+        write_stream(&mut stream, "HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+        write_stream(&mut stream, "HTTP/1.1 404 Not Found\r\n\r\n");
+    }
 }
 
 fn main() {
