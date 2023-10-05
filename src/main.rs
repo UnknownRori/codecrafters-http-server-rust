48f04f81718643554a08e57d14e10f615307378e
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-10-06 06:51:16 +0700

    pass 6th stage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use core::fmt;
-use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
 struct Request {
     user_agent: String,
@@ -76,18 +76,19 @@ fn get_method<'a>(lines: &'a Vec<&'a str>) -> Option<HttpMethod> {
     }
 }
 
-fn parse_request(stream: &mut TcpStream) -> Request {
+async fn parse_request(stream: &mut TcpStream) -> Request {
     let mut buffer: [u8; 512] = [0u8; 512];
-    let bytes_read = stream.read(&mut buffer).expect("Failed to read the stream");
+    let bytes_read = stream
+        .read(&mut buffer)
+        .await
+        .expect("Failed to read the stream");
 
     let request_header = String::from_utf8_lossy(&buffer[..bytes_read]).to_string();
 
     let lines = request_header.split("\r\n").collect::<Vec<&str>>();
 
     let path = get_path(&lines).expect("Failed to get path").to_string();
-    let user_agent = get_user_agent(&lines)
-        .expect("Failed to get user agent")
-        .to_string();
+    let user_agent = get_user_agent(&lines).unwrap_or("").to_string();
     let method = get_method(&lines).expect("Failed to get method");
 
     Request {
@@ -117,52 +118,50 @@ fn respond(http: HttpCode, content_type: ContentType, message: &str) -> String {
     response
 }
 
-fn write_stream(stream: &mut TcpStream, message: &str) {
+async fn write_stream(stream: &mut TcpStream, message: &str) {
     println!("send stream : {:#?}", message);
 
     stream
         .write(message.as_bytes())
+        .await
         .expect("Failed to send a response");
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let request = parse_request(&mut stream);
+async fn handle_connection(mut stream: TcpStream) {
+    let request = parse_request(&mut stream).await;
 
     println!("Got path: {}", request.path);
     if request.path.starts_with("/echo") {
         let content = request.path.replace("/echo/", "");
         let response = respond(HttpCode::Ok200, ContentType::TextPlain, &content);
-        write_stream(&mut stream, &response);
+        write_stream(&mut stream, &response).await;
     } else if request.path == "/user-agent" {
         let response = respond(
             HttpCode::Ok200,
             ContentType::TextPlain,
             request.user_agent.as_str(),
         );
-        write_stream(&mut stream, &response);
+        write_stream(&mut stream, &response).await;
     } else if request.path == "/" {
         let response = respond(HttpCode::Ok200, ContentType::None, "");
-        write_stream(&mut stream, &response);
+        write_stream(&mut stream, &response).await;
     } else {
         let response = respond(HttpCode::Err404, ContentType::None, "");
-        write_stream(&mut stream, &response);
+        write_stream(&mut stream, &response).await;
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let conn = "127.0.0.1:4221";
-    let listener = TcpListener::bind(&conn).unwrap();
+    let listener = TcpListener::bind(&conn).await.unwrap();
 
     println!("Server started at {}", &conn);
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("accepted new connection");
-                handle_connection(stream);
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            println!("accepted new connection");
+            handle_connection(stream).await;
+        });
     }
 }
