2667d4ec1e0c7ce0fb9bea0d668a934d8afa2bad
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-10-06 06:37:43 +0700

    pass 5th stage

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,17 @@ use core::fmt;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
+struct Request {
+    user_agent: String,
+    method: HttpMethod,
+    path: String,
+}
+
+enum HttpMethod {
+    Get,
+    Post,
+}
+
 enum HttpCode {
     Ok200,
     Err404,
@@ -34,6 +45,16 @@ impl fmt::Display for HttpCode {
     }
 }
 
+fn get_user_agent<'a>(lines: &'a Vec<&'a str>) -> Option<&'a str> {
+    for line in lines {
+        if line.starts_with("User-Agent") {
+            return line.split(": ").nth(1);
+        }
+    }
+
+    None
+}
+
 fn get_path<'a>(lines: &'a Vec<&'a str>) -> Option<&'a str> {
     for line in lines {
         if line.starts_with("GET") {
@@ -44,11 +65,36 @@ fn get_path<'a>(lines: &'a Vec<&'a str>) -> Option<&'a str> {
     None
 }
 
-fn parse_request(stream: &mut TcpStream) -> String {
+fn get_method<'a>(lines: &'a Vec<&'a str>) -> Option<HttpMethod> {
+    match lines[0].split(" ").nth(0) {
+        Some(val) => match val {
+            "GET" => Some(HttpMethod::Get),
+            "POST" => Some(HttpMethod::Post),
+            _ => None,
+        },
+        None => None,
+    }
+}
+
+fn parse_request(stream: &mut TcpStream) -> Request {
     let mut buffer: [u8; 512] = [0u8; 512];
     let bytes_read = stream.read(&mut buffer).expect("Failed to read the stream");
 
-    return String::from_utf8_lossy(&buffer[..bytes_read]).to_string();
+    let request_header = String::from_utf8_lossy(&buffer[..bytes_read]).to_string();
+
+    let lines = request_header.split("\r\n").collect::<Vec<&str>>();
+
+    let path = get_path(&lines).expect("Failed to get path").to_string();
+    let user_agent = get_user_agent(&lines)
+        .expect("Failed to get user agent")
+        .to_string();
+    let method = get_method(&lines).expect("Failed to get method");
+
+    Request {
+        path,
+        user_agent,
+        method,
+    }
 }
 
 fn respond(http: HttpCode, content_type: ContentType, message: &str) -> String {
@@ -80,17 +126,21 @@ fn write_stream(stream: &mut TcpStream, message: &str) {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let binding = parse_request(&mut stream);
-    let request_lines = binding.split("\r\n").collect::<Vec<&str>>();
+    let request = parse_request(&mut stream);
 
-    let path = get_path(&request_lines).expect("Failed to get path");
-
-    println!("Got path: {}", path);
-    if path.starts_with("/echo") {
-        let content = path.replace("/echo/", "");
+    println!("Got path: {}", request.path);
+    if request.path.starts_with("/echo") {
+        let content = request.path.replace("/echo/", "");
         let response = respond(HttpCode::Ok200, ContentType::TextPlain, &content);
         write_stream(&mut stream, &response);
-    } else if path == "/" {
+    } else if request.path == "/user-agent" {
+        let response = respond(
+            HttpCode::Ok200,
+            ContentType::TextPlain,
+            request.user_agent.as_str(),
+        );
+        write_stream(&mut stream, &response);
+    } else if request.path == "/" {
         let response = respond(HttpCode::Ok200, ContentType::None, "");
         write_stream(&mut stream, &response);
     } else {
